b3315234b9dbd0a8eb5d9227ef74027541a011df
Daniel Qi <drqi@gmail.com>
2019-12-28 01:01:24 -0800

    started working on franimal stuff

---
+++ Franimal Atsume/Assets/Scripts/Franimal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Franimal", menuName = "Franimal")]
+public class Franimal : ScriptableObject
+{
+    public AnimalSpecies species;
+    public AnimalLocation location;
+    public int MinSeeds;
+    public int MaxSeeds;
+    public int PowerLevel;
+    public int MinDuration;
+    public int MaxDuration;
+    public Toy FavoriteToy;
+}
---
+++ Franimal Atsume/Assets/Scripts/SpawnLocation.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLocation : MonoBehaviour
+{
+    public Toy currentToy;
+
+    bool hasFranimal = false;
+
+    Franimal currentFranimal;
+    float franimalTimeRemaining = 0;
+
+
+    float getNewFranimalTimer = 300f;
+    float getNewFranimalProbability = 0.25f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecrementCurrentFranimal();
+        CheckForNewFranimal();
+    }
+
+    void DecrementCurrentFranimal()
+    {
+        if (hasFranimal)
+        {
+            franimalTimeRemaining -= Time.deltaTime;
+        }
+        if(franimalTimeRemaining < 0f)
+        {
+
+        }
+    }
+
+    void CheckForNewFranimal()
+    {
+        if (getNewFranimalTimer <= 0)
+        {
+            if (Random.value < getNewFranimalProbability)
+            {
+                getNewFranimalTimer = 300f;
+                AttractNewFranimal();
+            }
+            else
+            {
+                getNewFranimalTimer -= Time.deltaTime;
+            }
+
+        }
+    }
+
+    void AttractNewFranimal()
+    {
+        if(currentToy != null)
+        {
+
+        }
+    }
+}
